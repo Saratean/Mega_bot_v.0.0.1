d7704ddd948c17dc21a25a812c5bbefc3a54eaa6
Saratean <135757567+Saratean@users.noreply.github.com>
2023-06-24 16:48:22 +0200

    Add files via upload

---
+++ BotD.py
@@ -0,0 +1,40 @@
+from settings import settings
+import discord
+
+from bot_logic import *
+
+
+
+# Переменная intents - хранит привилегии бота
+intents = discord.Intents.default()
+# Включаем привелегию на чтение сообщений
+intents.message_content = True
+# Создаем бота в переменной client и передаем все привелегии
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("ПР я Mega Bot!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send(":saluting_face:")
+    elif message.content.startswith('$smile'):
+        await message.channel.send(gen_emodji())
+    elif message.content.startswith('$coin'):
+        await message.channel.send(flip_coin())
+    elif message.content.startswith('$pass'):
+        await message.channel.send(gen_pass(10))
+    else:
+        await message.channel.send("Я не понимаю такую команду!")
+
+client.run(settings["TOKEN"])
+
+
+
+
